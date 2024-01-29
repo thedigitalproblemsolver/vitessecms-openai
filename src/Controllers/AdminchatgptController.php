63ed927d55fc996426703db9957cf0d9dcf877cf
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-01-29 12:15:01 +0100

    Added js listener and chatGPTController

--- src/Controllers/AdminchatgptController.php
+++ src/Controllers/AdminchatgptController.php
@@ -5,46 +5,49 @@ declare(strict_types=1);
 namespace VitesseCms\OpenAI\Controllers;
 
 use VitesseCms\Core\AbstractControllerAdmin;
+use VitesseCms\OpenAI\DTO\ConversationDTO;
+use VitesseCms\OpenAI\Forms\PromptForm;
 
 final class AdminchatgptController extends AbstractControllerAdmin
 {
-    public function promptAction(): void
+    public function promptFormAction()
     {
-        $api_key = getenv('CHATGPT_API_KEY');
+        $promptForm = new PromptForm();
+        $promptForm->buildForm();
 
-        $conversation = [
-            ["role" => "system", "content" => "You are a SEO specialist."],
-            [
-                "role" => "user",
-                "content" => "schrijf 3 alinea's voor een webshop over het product 'HEMA Nijntje Plakboek Blanco'"
-            ],
-        ];
+        $this->view->set('content', $promptForm->renderForm('admin/openai/adminchatgpt/handlepromptform'));
+    }
 
-        $data = [
-            'model' => 'gpt-3.5-turbo',
-            'messages' => $conversation,
-        ];
+    public function HandlePromptFormAction(): void
+    {
+        $promptForm = new PromptForm();
+        $promptForm->buildForm();
+        if ($promptForm->validate()) {
+            $conversationDTO = new ConversationDTO(
+                getenv('CHATGPT_API_KEY'),
+                'gpt-3.5-turbo',
+                'You are a SEO specialist.',
+                $this->request->getPost('prompt')
+            );
 
-        $headers = [
-            'Authorization: Bearer ' . $api_key,
-            'Content-Type: application/json',
-        ];
+            $ch = curl_init('https://api.openai.com/v1/chat/completions');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($conversationDTO->getCurlData()));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $conversationDTO->getCurlHeader());
 
-        $ch = curl_init('https://api.openai.com/v1/chat/completions');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            $response = curl_exec($ch);
+            curl_close($ch);
 
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $response_data = json_decode($response, true);
-        $assistant_reply = $response_data['choices'][0]['message']['content'];
-        echo "Assistant: " . $assistant_reply;
-
-        echo 'in prompt action';
-        die();
+            if (is_string($response)) {
+                $response_data = json_decode($response, true);
+                $assistant_reply = $response_data['choices'][0]['message']['content'];
+                $this->view->set('content', $assistant_reply);
+            } else {
+                $this->flashService->setError('Prompt failed');
+                $this->redirect($this->request->getHTTPReferer());
+            }
+        }
     }
 }
 
--- src/Resources/js/initOpenAIUI.js
+++ src/Resources/js/initOpenAIUI.js
@@ -1,7 +1,7 @@
 var initOpenAIUI = function () {
     for (const element of document.getElementsByClassName('editor')) {
         let span = document.createElement('span');
-        span.innerHTML = '<a class="fa fa-comments-o" href="admin/openai/adminchatgpt/prompt" target="_blank"></a>';
+        span.innerHTML = '<a class="fa fa-comments-o openmodal" href="admin/openai/adminchatgpt/promptform" target="_blank"></a>';
         span.className = 'input-group-prepend input-group-text';
         element.before(span);
     }
